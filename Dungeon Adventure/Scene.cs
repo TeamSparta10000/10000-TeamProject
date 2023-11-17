fcf6a2dc038e5a5602c500513149e72a399c1a9b
FrostKim0208 <admiral0208@naver.com>
2023-11-17 17:43:30 +0900

    Script Separation

---
+++ Dungeon Adventure/Scene.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Dungeon_Adventure;
+
+namespace Dungeon_Adventure
+{
+    internal class Scene
+    {
+        public static void DisplayGameStart()
+        {
+            Console.WriteLine(" ________      ___  ___      ________      _______       ________      ________      ________           ________      ________      ___      ___  _______       ________       _________    ___  ___      ________      _______      ");
+            Console.WriteLine("|\\   ___ \\    |\\  \\|\\  \\    |\\   ____\\    |\\  ___ \\     |\\   __  \\    |\\   __  \\    |\\   ___  \\        |\\   __  \\    |\\   ___ \\    |\\  \\    /  /||\\  ___ \\     |\\   ___  \\    |\\___   ___\\ |\\  \\|\\  \\    |\\   __  \\    |\\  ___ \\     ");
+            Console.WriteLine("\\ \\  \\_|\\ \\   \\ \\  \\\\\\  \\   \\ \\  \\___|    \\ \\   __/|    \\ \\  \\|\\  \\   \\ \\  \\|\\  \\   \\ \\  \\\\ \\  \\       \\ \\  \\|\\  \\   \\ \\  \\_|\\ \\   \\ \\  \\  /  / /\\ \\   __/|    \\ \\  \\\\ \\  \\   \\|___ \\  \\_| \\ \\  \\\\\\  \\   \\ \\  \\|\\  \\   \\ \\   __/|    ");
+            Console.WriteLine(" \\ \\  \\ \\\\ \\   \\ \\  \\\\\\  \\   \\ \\  \\  ___   \\ \\  \\_|/__   \\ \\   __  \\   \\ \\  \\\\\\  \\   \\ \\  \\\\ \\  \\       \\ \\   __  \\   \\ \\  \\ \\\\ \\   \\ \\  \\/  / /  \\ \\  \\_|/__   \\ \\  \\\\ \\  \\       \\ \\  \\   \\ \\  \\\\\\  \\   \\ \\   _  _\\   \\ \\  \\_|/__  ");
+            Console.WriteLine("  \\ \\  \\_\\\\ \\   \\ \\  \\\\\\  \\   \\ \\  \\|\\  \\   \\ \\  \\_|\\ \\   \\ \\  \\ \\  \\   \\ \\  \\\\\\  \\   \\ \\  \\\\ \\  \\       \\ \\  \\ \\  \\   \\ \\  \\_\\\\ \\   \\ \\    / /    \\ \\  \\_|\\ \\   \\ \\  \\\\ \\  \\       \\ \\  \\   \\ \\  \\\\\\  \\   \\ \\  \\\\  \\|   \\ \\  \\_|\\ \\ ");
+            Console.WriteLine("   \\ \\_______\\   \\ \\_______\\   \\ \\_______\\   \\ \\_______\\   \\ \\__\\ \\__\\   \\ \\_______\\   \\ \\__\\\\ \\__\\       \\ \\__\\ \\__\\   \\ \\_______\\   \\ \\__/ /      \\ \\_______\\   \\ \\__\\\\ \\__\\       \\ \\__\\   \\ \\_______\\   \\ \\__\\\\ _\\    \\ \\_______\\");
+            Console.WriteLine("   \\|_______|    \\|_______|    \\|_______|    \\|_______|    \\|__|\\|__|    \\|_______|    \\|__| \\|__|        \\|__|\\|__|    \\|_______|    \\|__|/        \\|_______|    \\|__| \\|__|        \\|__|    \\|_______|    \\|__|\\|__|    \\|_______|");
+            Console.WriteLine("=========================================================================================================================================================================================================================================================");
+            Console.WriteLine("                                                                                                               PRESS ANYKEY TO START                                                                                                                     ");
+            Console.WriteLine("=========================================================================================================================================================================================================================================================");
+            Console.ReadKey();
+
+
+
+        }
+        public static void DisplayTown()
+        {
+            Console.Clear();
+
+            Console.WriteLine("초보던전 입구마을에 오신" + (GameData.player.Name) + "님 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기 전에 점검을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+            int input = Program.CheckValidInput(1, 2);
+            switch (input)
+            {
+                case 1:
+                    Scene.DisplayMyInfo();
+                    break;
+
+                case 2:
+                    DisplayInventory();
+                    break;
+            }
+        }
+        public static void DisplayMyInfo()
+        {
+            Console.Clear();
+
+            Program.ShowHighlightText("상태보기");
+            Console.WriteLine("캐릭터의 정보를 표시합니다.");
+            Console.WriteLine();
+            Program.PrintTextWithHighlights($"Lv.", GameData.player.Level.ToString("00"));
+            Console.WriteLine($"{GameData.player.Name}({GameData.player.Job})");
+
+            int bonusAtk = GameData.GetSumBonusAtk();
+            int bonusDef = GameData.GetSumBonusDef();
+            int bonusHp = GameData.GetSumBonusHp();
+            int bonusMp = GameData.GetSumBonusMp();
+
+            Program.PrintTextWithHighlights($"Atk :", (GameData.player.Atk + bonusAtk).ToString(), bonusAtk > 0 ? string.Format("(+{0})", bonusAtk) : "");
+            Program.PrintTextWithHighlights($"Def :", (GameData.player.Def + bonusDef).ToString(), bonusDef > 0 ? string.Format("(+{0})", bonusDef) : "");
+            Program.PrintTextWithHighlights($"Hp :", (GameData.player.Hp + bonusHp).ToString(), bonusHp > 0 ? string.Format("(+{0})", bonusHp) : "");
+            Program.PrintTextWithHighlights($"Mp :", (GameData.player.Mp + bonusMp).ToString(), bonusMp > 0 ? string.Format("(+{0})", bonusMp) : "");
+            Console.WriteLine($"Gold : {GameData.player.Gold} G");
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+            int input = Program.CheckValidInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    DisplayTown();
+                    break;
+            }
+        }
+        public static void DisplayInventory()
+        {
+            Console.Clear();
+
+            Program.ShowHighlightText("인벤토리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine();
+            for (int i = 0; i < GameData.Item.ItemCnt; i++)
+            {
+                GameData.items[i].PrintItemDescription();
+            }
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("1. 장착관리");
+
+            int input = Program.CheckValidInput(0, 1);
+            switch (input)
+            {
+                case 0:
+                    DisplayTown();
+                    break;
+                case 1:
+                    EquipMenu();
+                    break;
+            }
+        }
+        public static void EquipMenu()
+        {
+            Console.Clear();
+
+            Program.ShowHighlightText("인벤토리 - 장착관리");
+            Console.WriteLine("보유중인 아이템을 장착하거나 해제할수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            for (int i = 0; i < GameData.Item.ItemCnt; i++)
+            {
+                GameData.items[i].PrintItemDescription(true, i + 1);
+            }
+            Console.WriteLine();
+            Console.WriteLine("0.나가기");
+            int keyinput = Program.CheckValidInput(0, GameData.Item.ItemCnt);
+            switch (keyinput)
+            {
+                case 0:
+                    DisplayInventory();
+                    break;
+                default:
+                    Program.ToggleEqupStatus(keyinput - 1);
+                    EquipMenu();
+                    break;
+            }
+        }
+    }
+}
