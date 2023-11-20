9046696ae224eb5d0207b347fbf901919d6e0738
FrostKim0208 <admiral0208@naver.com>
2023-11-20 21:25:19 +0900

    Add decide name ,job

--- Dungeon Adventure/GameData.cs
+++ Dungeon Adventure/GameData.cs
@@ -17,34 +17,29 @@ namespace Dungeon_Adventure
             Console.Write("캐릭터 이름을 입력하세요: ");
             string name = Console.ReadLine();
             Console.Clear();
-            static Character ChooseClass()
-            {
-                Job choice = Job.None;
-                while (true)
-                {
+           
                     Console.WriteLine("1.Knight(기사) 초기 스탯-Atk:10,Def:20,Hp:200,Mp:50,Gold:3000 ");
                     Console.WriteLine("2.Mage(마법사) 초기 스탯-Atk:20,Def:5,Hp:50,Mp:200,Gold:5000 ");
                     Console.WriteLine("3.Archer(궁수) 초기 스탯-Atk:15,Def:15,Hp:150,Mp:100,Gold:2000 ");
                     Console.Write("직업을 선택하세요: ");
+            int input = Program.CheckValidInput(1, 3);
+            switch (input)
+            {
+                case 1:
+                    player = new Character(name, "Knight", 1, 10, 20, 200, 50, 3000);
+                    break;
+                case 2:
+                    player = new Character(name, "Mage", 1, 20, 5, 50, 200, 5000);
+                    break;
+                case 3:
+                    player = new Character(name, "Archer", 1, 15, 15, 150, 100, 3000);
+                    break;
+            }
 
-                    string input = Console.ReadLine();
-                    switch (input)
-                    {
-                        case "1":
-                            choice = Character.Knight;
-                            break;
-                        case "2":
-                            choice = Character.Archer;
-                            break;
-                        case "3":
-                            choice = Character.Mage;
-                            break;
-                    }
-                    if (choice != Character.None)
-                        break;
-                }
-                return choice;
-                Console.Clear();
+
+            
+
+                    
             }
             public static void AddItem(Item item)
             {
@@ -90,4 +85,4 @@ namespace Dungeon_Adventure
             }
         }
     } 
-}
+
