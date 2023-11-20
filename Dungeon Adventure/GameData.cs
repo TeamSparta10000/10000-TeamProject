7b19c4e3bb86c83cd831d60c3000e7039da8c76d
FrostKim0208 <admiral0208@naver.com>
2023-11-20 10:59:23 +0900

    Add Character Script

--- Dungeon Adventure/GameData.cs
+++ Dungeon Adventure/GameData.cs
@@ -22,31 +22,31 @@ namespace Dungeon_Adventure
 
 
         }
-        public class Character
-        {
-            public string Name { get; }
-            public string Job { get; }
-            public int Level { get; }
-            public int Atk { get; }
-            public int Def { get; }
-            public int Hp { get; }
-            public int Mp { get; }
-            public int Gold { get; }
+        //public class Character
+        //{
+        //    public string Name { get; }
+        //    public string Job { get; }
+        //    public int Level { get; }
+        //    public int Atk { get; }
+        //    public int Def { get; }
+        //    public int Hp { get; }
+        //    public int Mp { get; }
+        //    public int Gold { get; }
 
-            public Character(string name, string job, int level, int atk, int def, int hp, int mp, int gold)
-            {
-                Name = name;
-                Job = job;
-                Level = level;
-                Atk = atk;
-                Def = def;
-                Hp = hp;
-                Mp = mp;
-                Gold = gold;
-            }
+        //    public Character(string name, string job, int level, int atk, int def, int hp, int mp, int gold)
+        //    {
+        //        Name = name;
+        //        Job = job;
+        //        Level = level;
+        //        Atk = atk;
+        //        Def = def;
+        //        Hp = hp;
+        //        Mp = mp;
+        //        Gold = gold;
+        //    }
 
 
-        }
+        //}
         public static void AddItem(Item item)
         {
             if (Item.ItemCnt == 10) return;
