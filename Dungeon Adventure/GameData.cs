95a1aab9bb3a0953f2eae5a88221c74c27e9b94c
FrostKim0208 <admiral0208@naver.com>
2023-11-21 16:09:44 +0900

    retouch

--- Dungeon Adventure/GameData.cs
+++ Dungeon Adventure/GameData.cs
@@ -5,12 +5,14 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Dungeon_Adventure;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Dungeon_Adventure
 {
     public class GameData
     {
         public static Character player;
+        public static Monster[] monsters;
         public static Item[] items;
         public static void GameDataSetting()
         {
@@ -47,13 +49,68 @@ namespace Dungeon_Adventure
                     AddItem(new Item("초급 궁수의 마법화살", "궁수에게 지급되는 마법화살이다.", 1, 10, 0, 0, 50));
                     break;
             }
+            monsters = new Monster[10];
+            MonsterList(new Monster("가고일", "마계에서 온 흔히 볼 수 있는 가고일입니다.", 12, 10, 40, 10));
+            MonsterList(new Monster("골렘", "마기가 가득한 돌덩이들이 모여 만들어졌습니다.", 5, 10, 60, 10));
+        }
 
-
-            
-
-                    
+        public static int PlayerAtkDamage() // Character class에 생성하고 싶었으나, script가 나누어져 호출 문제로 GameDate.cs에 생성함 Enemy 데이터가 만들어지면 EnemyAtkDamage() 메서드 생성 예정
+        {
+            // 아이템을 장착하면 캐릭터의 스탯에 반영한다.
+            double MinDamage = (GetSumBonusAtk() > 0 ? player.Atk + GetSumBonusAtk() : player.Atk) - Math.Ceiling((double)player.Atk / 10);
+            //double MinDamage = player.Atk - Math.Ceiling((double)player.Atk / 10);
+            double MaxDamage = (GetSumBonusAtk() > 0 ? player.Atk + GetSumBonusAtk() : player.Atk) + Math.Ceiling((double)player.Atk / 10);
+            //double MaxDamage = player.Atk + Math.Ceiling((double)player.Atk / 10);
+            int atkDamage = new Random().Next((int)MinDamage, (int)MaxDamage + 1);
+            return atkDamage;
+        }
+        public static void PlayerTakeDamage(int damage) // 이하동문
+        {
+            player.Hp -= damage;
+            if (player.Hp <= 0)
+            {
+                Console.WriteLine($"{player.Name}이(가) 죽었습니다.");
             }
-            public static void AddItem(Item item)
+            else
+            {
+                Console.WriteLine($"{player.Name}이(가) {damage}의 데미지를 받았습니다. 남은 체력: {player.Hp}");
+            }
+        }
+        public static int MonsterAtkDamage(int i) // 마찬가지의 이유로 GameDate.cs에 생성항. 
+        {
+            double MinDamage = monsters[i].Atk - Math.Ceiling((double)monsters[i].Atk / 10);
+            double MaxDamage = monsters[i].Atk + Math.Ceiling((double)monsters[i].Atk / 10);
+            int atkDamage = new Random().Next((int)MinDamage, (int)MaxDamage + 1);
+            return atkDamage;
+        }
+
+        public static void MonsterTakeDamage(int damage, int i) // 이하동문
+        {
+            monsters[i].Hp -= damage;
+            if (monsters[i].Hp <= 0)
+            {
+                Program.MonsterDead(i);
+                monsters[i].Hp = 0;
+                Console.WriteLine($"{monsters[i].MonsterName}이(가) 죽었습니다.");
+            }
+            else
+            {
+                Console.WriteLine($"{monsters[i].MonsterName}이(가) {damage}의 데미지를 받았습니다. 남은 체력: {monsters[i].Hp}");
+            }
+        }
+
+        public static void MonsterList(Monster monster)
+        {
+            if (Monster.MonsterCnt == 10) return;
+            monsters[Monster.MonsterCnt] = monster;
+            Monster.MonsterCnt++;
+
+
+
+
+
+        }
+        public static void AddItem(Item item)
             {
                 if (Item.ItemCnt == 10) return;
                 items[Item.ItemCnt] = item;
@@ -64,7 +121,7 @@ namespace Dungeon_Adventure
                 int sum = 0;
                 for (int i = 0; i < Item.ItemCnt; i++)
                 {
-                    if (GameData.items[i].IsEquipped) sum += GameData.items[i].Atk;
+                    if (items[i].IsEquipped) sum += items[i].Atk;
                 }
                 return sum;
             }
@@ -73,7 +130,7 @@ namespace Dungeon_Adventure
                 int sum = 0;
                 for (int i = 0; i < Item.ItemCnt; i++)
                 {
-                    if (GameData.items[i].IsEquipped) sum += GameData.items[i].Def;
+                    if (items[i].IsEquipped) sum += items[i].Def;
                 }
                 return sum;
             }
@@ -82,7 +139,7 @@ namespace Dungeon_Adventure
                 int sum = 0;
                 for (int i = 0; i < Item.ItemCnt; i++)
                 {
-                    if (GameData.items[i].IsEquipped) sum += GameData.items[i].Hp;
+                    if (items[i].IsEquipped) sum += items[i].Hp;
                 }
                 return sum;
             }
@@ -91,7 +148,7 @@ namespace Dungeon_Adventure
                 int sum = 0;
                 for (int i = 0; i < Item.ItemCnt; i++)
                 {
-                    if (GameData.items[i].IsEquipped) sum += GameData.items[i].Mp;
+                    if (items[i].IsEquipped) sum += items[i].Mp;
                 }
                 return sum;
             }
